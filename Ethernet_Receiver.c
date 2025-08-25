0088a5e224cc40916293d8a05b35c309e6895168
koren23 <79501333+koren23@users.noreply.github.com>
2025-08-25 13:58:30 +0300

    Update Ethernet_Receiver.c

--- Ethernet_Receiver.c
+++ Ethernet_Receiver.c
@@ -1,107 +1,90 @@
 #include "xparameters.h"
 #include "xil_printf.h"
-
 #include "lwip/udp.h" // lwIP UDP API definitions
 #include "lwip/init.h" // lwIP stack initialization
 #include "lwip/ip_addr.h" // IP address structure and helpers
 #include "lwip/pbuf.h" // packet buffer structure
 #include "netif/xadapter.h" // ethernet adapter ethernet for lwIP
 
-#define LISTEN_PORT 12345 // port number to listen for incoming UDP packets
+#define LISTEN_PORT 12345 // port chosen 
 
-// global UDP protocol control block for receiving data
-struct udp_pcb *receiver_pcb;
+struct udp_pcb *receiver_pcb; // pointer to UDP control block used to manage and receive packets
 
-// network interface structure => represents the ethernet interface
-struct netif server_netif;
+struct netif server_netif; // variable type struct netif
 
-// MAC address of the board => located behind the board
-u8_t mac_address[6] = {0x00, 0x18, 0x3E, 0x04, 0x81, 0xD6};
+u8_t mac_address[6] = {0x00, 0x18, 0x3E, 0x04, 0x81, 0xD6}; // artyz7-10 mac address
 
-// utility functiomn to print an IP address with a label
 void print_ip(const char *msg, ip_addr_t *ip) {
     xil_printf("%s: %d.%d.%d.%d\n", msg, ip4_addr1(ip), ip4_addr2(ip), ip4_addr3(ip), ip4_addr4(ip));
 }
 
-// callback function triggered when UDP packet arrives => extracts the message from the pbuffer
-void udp_receive_callback(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port) {
+void udp_receive_callback(void *arg, // a value i can set so itll send it back when called - not used
+                          struct udp_pcb *pcb, // contains the lwip state for this udp - isnt used cos not replying
+                          struct pbuf *p, const ip_addr_t *addr, // sender address
+                          u16_t port) {
     (void)arg;
     (void)pcb;
 
     if (p != NULL) {
-        char msg[256] = {0}; // buffer to store received message
-        size_t len = (p->len < sizeof(msg) - 1) ? p->len : sizeof(msg) - 1;
+        char msg[256] = {0};
+        // if length of the pbuffer data is bigger than 255 it sets it as 255 to avoid overflow
+        size_t len = (p->len < sizeof(msg) - 1) ? p->len : sizeof(msg) - 1; 
 
-        // copy payload to buffer and null-terminate
-        memcpy(msg, p->payload, len);
-        msg[len] = '\0';
-
-        // print source IP, port, message received.
+        memcpy(msg, p->payload, len); // copies len bytes from pbuff to msg
+        msg[len] = '\0'; // sets a null terminator
         xil_printf("Received from %d.%d.%d.%d:%d -> %s\n", ip4_addr1(addr), ip4_addr2(addr), ip4_addr3(addr), ip4_addr4(addr), port, msg);
 
-        // free the received packet buffer
-        pbuf_free(p);
+        pbuf_free(p); // frees the pbuffer
     }
 }
 
-// initialize the UDP receiver by creating a PCB and binding it to the listen port => sets the callback function for received packets
 void udp_receiver_init(){
-    receiver_pcb = udp_new(); // create new UDP PCB
+    receiver_pcb = udp_new(); // creates a new UDP protocol control block
     if (!receiver_pcb) {
         xil_printf("Failed to create receiver PCB\n");
         return;
     }
 
-    // bind PCB to all IP addresses on the specified port
-    err_t err = udp_bind(receiver_pcb, IP_ADDR_ANY, LISTEN_PORT);
+    err_t err = udp_bind(receiver_pcb, IP_ADDR_ANY, LISTEN_PORT); // listens to new packets
     if (err != ERR_OK) {
         xil_printf("UDP bind failed with error %d\n", err);
         return;
     }
 
-    // register callback function to handle incoming UDP packets
-    udp_recv(receiver_pcb, udp_receive_callback, NULL);
+    udp_recv(receiver_pcb, udp_receive_callback, NULL); // udp_recv calls udp_receive_callback with all its parameters from receiver_pcb
     xil_printf("UDP receiver initialized on port %d\n", LISTEN_PORT);
 }
 
-// intializes the network interface stetsup lwIP stack and continuously processes incoming ethernet frames
-int main() {
-    ip_addr_t ipaddr, netmask, gw; // local IP configuration tothe board
 
+int main() {
+    ip_addr_t ipaddr, netmask, gw; // declaration of 3 variables ... ip_addr_t is a struct from lwIP
     xil_printf("Starting lwIP UDP Receiver Example\n");
 
     IP4_ADDR(&ipaddr, 192, 168, 0, 27); // board IP address
     IP4_ADDR(&netmask, 255, 255, 255, 0); // subnet mask
     IP4_ADDR(&gw, 0, 0, 0, 0); // gateway address
 
-    // initialize lwIP stack
-    lwip_init();
+    lwip_init(); // lwIP function that resets everything there
 
-    struct netif *netif = &server_netif;
+    struct netif *netif = &server_netif; // declaration of netif, a pointer to server_netif
 
-    // add and intialize ethernet interface
-    if (!xemac_add(netif, &ipaddr, &netmask, &gw, mac_address, 0xe000b000)) {
+//  function the restarts the ethernet interface
+//  using the pointer it transfers that data into server_netif
+    if (!xemac_add(netif, &ipaddr, &netmask, &gw, mac_address, 0xe000b000)) { 
         xil_printf("Error adding network interface\n");
         return -1;
     }
 
-    // set the interface as default and bring it up
-    netif_set_default(netif);
-    netif_set_up(netif);
+    netif_set_default(netif); // sets netif as the default network interface
+    netif_set_up(netif); // marks the network interface as active
 
-    // check link status and print result
     xil_printf("Link is %s\n", netif_is_link_up(netif) ? "up" : "down");
-
-    // print board IP address
     print_ip("Board IP", &ipaddr);
 
-    //initialize UDP receiver (bind and set callback)
     udp_receiver_init();
 
-    // handle incoming ethernet frames
     while (1) {
-        xemacif_input(netif); // poll for incoming packets
+        xemacif_input(netif);
     }
-
     return 0;
 }
