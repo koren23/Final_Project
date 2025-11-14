aa9616c92a33a8b508bb5d1c1dddf0233f8e5208
koren23 <79501333+koren23@users.noreply.github.com>
2025-11-14 22:47:12 +0200

    Update communicating_with_dw.vhd

--- communicating_with_dw.vhd
+++ communicating_with_dw.vhd
@@ -2,122 +2,156 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity test_octet_id is
-    Port (
-        MISO    : in STD_LOGIC;        -- SPI Data from DW1000
-        CLK     : in STD_LOGIC;        -- System Clock 20MHZ
-        BTN     : in STD_LOGIC;        -- Start button
-        LED     : out STD_LOGIC_VECTOR(1 DOWNTO 0);       -- LED indicator
-        CS      : out STD_LOGIC;       -- SPI Chip Select
-        MOSI    : out STD_LOGIC;        -- SPI Data to DW1000
-        DATA_VECTOR : out std_logic_vector(39 downto 0)
+entity transmitter is
+    generic (
+        read_id_reg : STD_LOGIC_VECTOR(7 downto 0)  :="00000000"
     );
-end test_octet_id;
+    Port (
+        MISO        : in  STD_LOGIC;
+        MOSI        : out STD_LOGIC;
+        CSn         : out STD_LOGIC;
+        SPICLOCK    : out STD_LOGIC;                         -- SPI clock (10 MHz)
+        
+        CLOCK       : in  STD_LOGIC;                        -- 100MHz system clock
+        BUTTON      : in  STD_LOGIC;                        -- start button (trigger)
+        DOUT        : out STD_LOGIC_VECTOR(39 downto 0)    -- data received from dw1000
+    );
+end transmitter;
 
-architecture Behavioral of test_octet_id is
-
-    type state_type is (IDLE, READ_ID, RECEIVE_DATA0, WRITE_BUFFER, SEND_FRAME_CONTROL, READ_FRAME_CONTROL, RECEIVE_DATA1, DONE);
-    signal state       : state_type := IDLE;
-
-    signal bit_cnt     : integer range 0 to 47 := 0;
-    signal data        : std_logic_vector(39 downto 0) :=(others => '0');
+architecture Behavioral of transmitter is
+-- state machine
+    type state_type is (
+        IDLE,            -- wait for button
+        DELAY,           -- delay
+        SEND_BYTE,       -- send 1 byte
+        RECEIVE,
+        DONE
+    );
+    signal state          : state_type := IDLE;
     
-    constant id_read              : std_logic_vector(7 downto 0) := "00000000";
     
-    constant buffer_write         : std_logic_vector(15 downto 0) := "1000100101011010";
+    signal clock_counter  : integer range 0 to 9            := 0;       -- divide 100 MHz by 10 = 10 MHz
+    signal bit_count      : integer range 0 to 255          :=0;
+    signal current_byte   : std_logic_vector(7 downto 0)    := (others => '0' );
+-- signals for delay    
+    signal delay_counter : integer := 0;
+    signal delay_target  : integer := 0;
+    signal return_state  : state_type := IDLE;
     
-    constant frame_control_write  : std_logic_vector(47 downto 0) := "100010000000001101000000000000100000000000000000"; 
-    constant frame_control_read   : std_logic_vector(7 downto 0) := "00001000";
-
-
+    signal loop_check          : boolean :=false; -- used to create a delay when cs=1
+    
+    function delay_done(
+        counter : integer;
+        target  : integer
+    ) return boolean is
+    begin
+        return (counter >= target);
+    end function;
+    
 begin
 
- 
-    process(CLK)
+    process (CLOCK)
     begin
-        if rising_edge(CLK) then
-            case state is
-                when IDLE =>
-                    LED <= "00";
-                    CS  <= '1';
-                    MOSI <= '1';
-                    bit_cnt <= 0;
-                    if BTN = '1' then
-                        state <= READ_ID;
-                        LED <= "11";
-                    end if;
-                 
-                when READ_ID =>
-                    LED <= "00";
-                    CS  <= '0';
-                    MOSI <= id_read(7 - bit_cnt); 
-                    if bit_cnt = 7 then
-                        bit_cnt <= 0;
-                        state <= RECEIVE_DATA0;
-                        LED <= "11";
-                    else
-                        bit_cnt <= bit_cnt + 1;
-                    end if;
-                
-                when RECEIVE_DATA0 =>
-                    LED <= "00";
-                    MOSI <= '0';
-                    if bit_cnt = 31 then
-                        state <= SEND_FRAME_CONTROL; -- skipped buffer to test code
-                        bit_cnt <= 0;
-                        LED <= "11";
-                        data_vector <= (others => '0');
-                        data_vector <= data;
-                    else
-                        bit_cnt <= bit_cnt + 1;
-                        data(31 - bit_cnt) <= MISO;
-                    end if; 
-               
-                when SEND_FRAME_CONTROL =>
-                    LED <= "00";
-                    MOSI <= frame_control_write(47 - bit_cnt);
-                    if bit_cnt = 47 then
-                        bit_cnt <= 0;
-                        state <= READ_FRAME_CONTROL;
-                        LED <= "11";
-                    else
-                        bit_cnt <= bit_cnt + 1;
-                    end if;
-
-                when READ_FRAME_CONTROL =>
-                    LED <= "00";
-                    MOSI <= frame_control_read(7 - bit_cnt); 
-                    if bit_cnt = 7 then
-                        bit_cnt <= 0;
-                        state <= RECEIVE_DATA1;
-                        LED <= "11";
-                    else
-                        bit_cnt <= bit_cnt + 1;
-                    end if;
-
-                when RECEIVE_DATA1 =>
-                    LED <= "00";
-                    MOSI <= '0';
-                    if bit_cnt = 39 then
-                        CS  <= '1';
-                        bit_cnt <= 0;
-                        data_vector <= (others => '0');
-                        data_vector <= data;
-                        state <= DONE;
-                        LED <= "11";
-                    else
-                        bit_cnt <= bit_cnt + 1;
-                        data(47 - bit_cnt) <= MISO;
-                    end if;
+    
+        if rising_edge(CLOCK) then
+        case state is
+         ------------------------------------ 
+            when IDLE =>            -- cs=1 until button pressed then mosi=reg(msb), go to delay 3clks
+                CSn <= '1';
+                if BUTTON = '1' then
+                    CSn <= '0';
+                    MOSI <= read_id_reg(7); -- send last bit
                     
-                when DONE =>
-                    LED <= "00";
-                    CS  <= '1';
+                    current_byte <= read_id_reg;
+                    delay_target <= 3; -- call delay func
+                    return_state <= SEND_BYTE;
+                    state <= DELAY;
+                end if;
+        ------------------------------------
+            when DELAY =>           -- call delay function check if counter reached limit, return to next state.
+                if delay_done(delay_counter, delay_target) then
+                    delay_counter <= 0;
+                    state <= return_state;
+                else
+                    delay_counter <= delay_counter + 1;
+                end if;
+        ------------------------------------ 
+            when SEND_BYTE =>
+                if bit_count < 8 then
+                    if clock_counter = 0 then --  rising edge clock
+                        SPICLOCK <= '1';
+                    elsif clock_counter = 4 then -- falling edge clock
+                        SPICLOCK <= '0';
+                        MOSI <= current_byte(7 - bit_count);
+                    elsif clock_counter = 9 then
+                        bit_count <= bit_count + 1;
+                    end if;
+                    clock_counter <= clock_counter + 1;    
+                    if clock_counter = 9 then -- reset counter
+                        clock_counter <= 0;
+                    end if;
+                else
+                    bit_count <= 0; -- reset counters
+                    clock_counter <= 0;
                     
-                when others =>
-                    state <= IDLE;
-            end case;
+                    -- go to delay 4clks then receive
+                    delay_target <= 4;
+                    state <= DELAY;
+                    SPICLOCK <= '0';
+                    return_state <= RECEIVE;
+                end if;
+        ------------------------------------
+             when RECEIVE =>
+                if bit_count < 32 then
+                    if clock_counter = 0 then --  rising edge clock
+                        SPICLOCK <= '1';
+                        DOUT(31 - bit_count) <= MISO;    -- enter data to vector
+                    elsif clock_counter = 4 then -- falling edge clock
+                        SPICLOCK <= '0';
+                    elsif clock_counter = 9 then
+                        bit_count <= bit_count + 1;
+                    end if;
+                    clock_counter <= clock_counter + 1;    
+                    if clock_counter = 9 then -- reset counter
+                        clock_counter <= 0;
+                    end if;
+                else
+                    bit_count <= 0; -- reset counters
+                    clock_counter <= 0;
+                    SPICLOCK <= '0';
+                    
+                    -- go to delay 4clks then receive
+                    delay_target <= 4;
+                    state <= DELAY;
+                    return_state <= DONE;
+                end if;
+        ------------------------------------
+            when DONE => -- go to delay once then back to operation
+                if loop_check = false then -- start a loop
+                    CSn <= '1';
+                    delay_target <= 4;
+                    state <= DELAY;
+                    return_state <= DONE;
+                    loop_check <= true;
+                else            -- go to delay => send_byte, etc
+                    CSn <= '0';
+                    loop_check <= false; 
+                    delay_target <= 3; -- call delay func
+                    return_state <= SEND_BYTE;
+                    state <= DELAY;
+                end if;
+        ------------------------------------
+            
+        end case;
         end if;
+        
+
     end process;
 
 end Behavioral;
+
+
+--  when reading a reg it goes
+--  idle => delay => send_byte => delay => receive => delay
+--  when writing data to a reg its
+--  idle => delay => (send byte => delay)x5 => delay
