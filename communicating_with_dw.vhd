c2b5e684f03da63a0488c745a11f2298ca61f036
koren23 <79501333+koren23@users.noreply.github.com>
2025-11-14 23:24:51 +0200

    Update communicating_with_dw.vhd

--- communicating_with_dw.vhd
+++ communicating_with_dw.vhd
@@ -4,7 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity transmitter is
     generic (
-        read_id_reg : STD_LOGIC_VECTOR(7 downto 0)  :="00000000"
+        read_id_reg : STD_LOGIC_VECTOR(7 downto 0)  :="00001111"
     );
     Port (
         MISO        : in  STD_LOGIC;
@@ -33,10 +33,12 @@ architecture Behavioral of transmitter is
     signal clock_counter  : integer range 0 to 9            := 0;       -- divide 100 MHz by 10 = 10 MHz
     signal bit_count      : integer range 0 to 255          :=0;
     signal current_byte   : std_logic_vector(7 downto 0)    := (others => '0' );
+    signal data_vector    : std_logic_vector(39 downto 0)   := (others => '0' );
 -- signals for delay    
     signal delay_counter : integer := 0;
     signal delay_target  : integer := 0;
     signal return_state  : state_type := IDLE;
+    signal resetMOSI     : boolean    := false;
     
     signal loop_check          : boolean :=false; -- used to create a delay when cs=1
     
@@ -62,6 +64,7 @@ begin
                     CSn <= '0';
                     MOSI <= read_id_reg(7); -- send last bit
                     
+                    resetMOSI <= false;
                     current_byte <= read_id_reg;
                     delay_target <= 3; -- call delay func
                     return_state <= SEND_BYTE;
@@ -69,6 +72,11 @@ begin
                 end if;
         ------------------------------------
             when DELAY =>           -- call delay function check if counter reached limit, return to next state.
+                if resetMOSI <= true then
+                    MOSI <= '0';
+                else
+                    resetMOSI <= true;
+                end if;
                 if delay_done(delay_counter, delay_target) then
                     delay_counter <= 0;
                     state <= return_state;
@@ -82,7 +90,7 @@ begin
                         SPICLOCK <= '1';
                     elsif clock_counter = 4 then -- falling edge clock
                         SPICLOCK <= '0';
-                        MOSI <= current_byte(7 - bit_count);
+                        MOSI <= current_byte(6 - bit_count);
                     elsif clock_counter = 9 then
                         bit_count <= bit_count + 1;
                     end if;
@@ -99,13 +107,14 @@ begin
                     state <= DELAY;
                     SPICLOCK <= '0';
                     return_state <= RECEIVE;
+                    data_vector <= (others => '0' );
                 end if;
         ------------------------------------
              when RECEIVE =>
                 if bit_count < 32 then
                     if clock_counter = 0 then --  rising edge clock
                         SPICLOCK <= '1';
-                        DOUT(31 - bit_count) <= MISO;    -- enter data to vector
+                        data_vector(31 - bit_count) <= MISO;    -- enter data to vector
                     elsif clock_counter = 4 then -- falling edge clock
                         SPICLOCK <= '0';
                     elsif clock_counter = 9 then
@@ -119,7 +128,7 @@ begin
                     bit_count <= 0; -- reset counters
                     clock_counter <= 0;
                     SPICLOCK <= '0';
-                    
+                    dout <= data_vector;
                     -- go to delay 4clks then receive
                     delay_target <= 4;
                     state <= DELAY;
