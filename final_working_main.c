bcf0084e5644c2210b8dbc3a65b6e63e1d1bbe68
koren23 <79501333+koren23@users.noreply.github.com>
2025-12-01 08:49:24 +0200

    Update final_working_main.c

--- final_working_main.c
+++ final_working_main.c
@@ -1,131 +1,220 @@
 #include "xparameters.h"
-#include "xil_printf.h"
-#include "lwip/udp.h" // lwIP UDP API definitions
-#include "lwip/init.h" // lwIP stack initialization
-#include "lwip/ip_addr.h" // IP address structure and helpers
-#include "lwip/pbuf.h" // packet buffer structure
-#include "netif/xadapter.h" // ethernet adapter ethernet for lwIP
 #include "xgpio.h"
+#include "xil_printf.h"
 #include "sleep.h"
+#include <time.h>
+#include "lwip/udp.h"
+#include "lwip/init.h"
+#include "lwip/ip_addr.h"
+#include "lwip/pbuf.h"
+#include "netif/xadapter.h"
+#define LISTEN_PORT 12345 // port chosen - needs to be changed
+#define BUFFER_SIZE 4096 // log max size
+#define Max_Size_Per_Message 256 // log max size per message
 
-#define LISTEN_PORT 12345 // port chosen 
-
-XGpio gpio;
 
 
-struct udp_pcb *receiver_pcb; // pointer to UDP control block used to manage and receive packets
+// global variables
+XGpio gpio; // gpio points to the XGpio struct supplied by xilinx 
+            // (containts base address pins state configs(input or output) etc)
 
-struct netif server_netif; // variable type struct netif
-
+struct udp_pcb *receiver_pcb; // receiver_pcb points to a udp_pcb - contains port num local-ip
+                        // it points to a callback function that activates when receiving data
+                        
+struct netif server_netif; // server_netif points to netif (contains  ip subnet gateway mac etc)
 u8_t mac_address[6] = {0x00, 0x18, 0x3E, 0x04, 0x81, 0xD6}; // artyz7-10 mac address
+static char message_buffer[BUFFER_SIZE] = {0};
+char tempstring[Max_Size_Per_Message] = {0}; 
 
-void print_ip(const char *msg, ip_addr_t *ip) {
-    xil_printf("%s: %d.%d.%d.%d\n", msg, ip4_addr1(ip), ip4_addr2(ip), ip4_addr3(ip), ip4_addr4(ip));
+
+
+void log_printer(const char *data_string){ // in charge of adding new data to previous and sending it in
+                                           // the format of a nextion command for log
+    char temp[Max_Size_Per_Message];
+    snprintf(temp, sizeof(temp), "%s\\r" ,data_string); // saves \r = enter + string to temp
+    size_t needed = strlen(temp); 
+    size_t current = strlen(message_buffer);
+    if (current + needed >= BUFFER_SIZE) {
+        message_buffer[0] = '\0'; // check if theres room in buffer
+    }  
+    strncat(message_buffer, temp, BUFFER_SIZE - strlen(message_buffer) - 1);   // saves old and new data to one string
+    xil_printf("log.txt=\"%s\"%c%c%c", message_buffer, 0xFF, 0xFF, 0xFF);  
+} 
+
+
+
+void print_ip(const char *msg, ip_addr_t *ip) { // gets called in general_initialization
+    snprintf(tempstring, sizeof(tempstring), "%s: %d.%d.%d.%d", msg, 
+                                                                ip4_addr1(ip), ip4_addr2(ip), 
+                                                                ip4_addr3(ip), ip4_addr4(ip));
+    log_printer(tempstring);
 }
 
-void pl_transmitter(char msg[256]){
-    u32 currtime, imptime, latval, longval;
-    for (int i = 0; msg[i] != '\0'; i++) {
-        if(i<32){currtime = (u32)msg[i];} 
-        else if(i<64){imptime = (u32)msg[i-32];}
-        else if(i<96){latval = (u32)msg[i-64];}
-        else if(i<128){longval = (u32)msg[i-96];}
-    }
 
-    XGpio_DiscreteWrite(&gpio, 1, 0x1);
+
+void format_timestamp(u32 timestamp, char *buffer, size_t buffer_size) { // convert unix data to time
+    // called in pl_transmitter
+    time_t raw_time = (time_t)(long)timestamp; // converts the value of timestamp to long and to time_t (for gmtime)
+    struct tm *tm_info = gmtime(&raw_time); // converts unix time to a struct (tm)
+
+    int year   = tm_info->tm_year + 1900; //time_t counts time since 1900
+    int month  = tm_info->tm_mon + 1; // counts from 0 so i added 1
+    int day    = tm_info->tm_mday;
+    int hour   = tm_info->tm_hour;
+    int minute = tm_info->tm_min;
+    int second = tm_info->tm_sec;
+    snprintf(buffer, buffer_size, "%02d/%02d/%04d %02d:%02d:%02d",
+             day, month, year, hour, minute, second);
+}
+
+
+
+void pl_transmitter(char msg[256]){ // called in udp_receive_callback
+    u32 currtime, imptime, latval, longval; // save data from msg to u32
+
+    //split msg to currtime imptime latval and longval
+    memcpy(&currtime, msg, 4);
+    memcpy(&imptime, msg + 4, 4);
+    memcpy(&latval, msg + 8, 4);
+    memcpy(&longval, msg + 12, 4);
+
+    xil_printf("page 0%c%c%c",0xFF,0xFF,0xFF); // go to page 0 - data page
+
+    char currstr[32];
+    format_timestamp(currtime, currstr, sizeof(currstr));// convert unix to display time
+    xil_printf("curt.txt=\"%s\"%c%c%c",currstr,0xFF,0xFF,0xFF);
+
+    char impstr[32];
+    format_timestamp(imptime, impstr, sizeof(impstr));// convert unix to display time
+    xil_printf("impt.txt=\"%s\"%c%c%c",impstr,0xFF,0xFF,0xFF);
+    
+    xil_printf("landmark.txt=\"(%d.%03d,%d.%03d)\"%c%c%c", latval / 1000, latval % 1000,longval / 1000, longval % 1000,0xFF, 0xFF, 0xFF);
+
+    
+    XGpio_DiscreteWrite(&gpio, 1, 0x1);// flag 1
     XGpio_DiscreteWrite(&gpio, 2, currtime);
-    xil_printf("Current time:\t%u\n", currtime);
+    snprintf(tempstring, sizeof(tempstring), "Current time:\t%u", currtime);
+    log_printer(tempstring);
+
     usleep(10);
-    XGpio_DiscreteWrite(&gpio, 1, 0x2);
+
+    XGpio_DiscreteWrite(&gpio, 1, 0x2); // flag 2
     XGpio_DiscreteWrite(&gpio, 2, imptime);
-    xil_printf("Impact time:\t%u\n", imptime);
+    snprintf(tempstring, sizeof(tempstring), "Impact time:\t%u", imptime);
+    log_printer(tempstring);
+
     usleep(10);
-    XGpio_DiscreteWrite(&gpio, 1, 0x4);
+
+    XGpio_DiscreteWrite(&gpio, 1, 0x4); // flag 4 (3 will be radius)
     XGpio_DiscreteWrite(&gpio, 2, latval);
-    xil_printf("Latitude:\t%.3f\n", (double)latval / 1000);
+    snprintf(tempstring, sizeof(tempstring), "Latitude:\t%.3f", (double)latval / 1000);
+    log_printer(tempstring);
+    
     usleep(10);
-    XGpio_DiscreteWrite(&gpio, 1, 0x5);
+
+    XGpio_DiscreteWrite(&gpio, 1, 0x5); // flag 5
     XGpio_DiscreteWrite(&gpio, 2, longval);
-    xil_printf("Longitude:\t%.3f\n", (double)longval / 1000);
+    snprintf(tempstring, sizeof(tempstring), "Longitude:\t%.3f", (double)longval / 1000);
+    log_printer(tempstring);
+
     usleep(10);
-    xil_printf("Data uploaded to PL ^_^");
+
+    log_printer("Data uploaded to PL ^_^");
+
     usleep(10);
     // clear valid flag
-    XGpio_DiscreteWrite(&gpio, 1, 0);
-    xil_printf("Flag cleared\n");
+    XGpio_DiscreteWrite(&gpio, 1, 0);// clear flag (0)
+    log_printer("Flag clear");
 }
 
+// defined in udp_receiver_init in udp_recv() as a callblack function
 void udp_receive_callback(void *arg, // a value i can set so itll send it back when called - not used
                           struct udp_pcb *pcb, // contains the lwip state for this udp - isnt used cos not replying
-                          struct pbuf *p, const ip_addr_t *addr, // sender address
+                          struct pbuf *p, // struct used to store network packet in memory
+                          const ip_addr_t *addr, // sender address
                           u16_t port) {
+    // unused
     (void)arg;
     (void)pcb;
 
-    if (p != NULL) {
+    if (p != NULL) {// if p !=NULL then theres data available
         char msg[256] = {0};
         // if length of the pbuffer data is bigger than 255 it sets it as 255 to avoid overflow
         size_t len = (p->len < sizeof(msg) - 1) ? p->len : sizeof(msg) - 1; 
 
         memcpy(msg, p->payload, len); // copies len bytes from pbuff to msg
         msg[len] = '\0'; // sets a null terminator
-        xil_printf("Received from %d.%d.%d.%d:%d -> %s\n", ip4_addr1(addr), ip4_addr2(addr), ip4_addr3(addr), ip4_addr4(addr), port, msg);
+        snprintf(tempstring, sizeof(tempstring), "Received from %d.%d.%d.%d:%d -> %s", 
+                                            ip4_addr1(addr), ip4_addr2(addr), ip4_addr3(addr), 
+                                            ip4_addr4(addr), port, msg);
+        log_printer(tempstring);
         pl_transmitter(msg);
         pbuf_free(p); // frees the pbuffer
     }
 }
 
-void udp_receiver_init(){
-    receiver_pcb = udp_new(); // creates a new UDP protocol control block
+void udp_receiver_init(){// called in main
+    receiver_pcb = udp_new();// creates a struct (receiver_pcb) with ip port and callback func
     if (!receiver_pcb) {
-        xil_printf("Failed to create receiver PCB\n");
+        log_printer("Failed to create receiver PCB");
         return;
     }
 
-    err_t err = udp_bind(receiver_pcb, IP_ADDR_ANY, LISTEN_PORT); // listens to new packets
+    err_t err = udp_bind(receiver_pcb, IP_ADDR_ANY, LISTEN_PORT);  // udp_bind() makes receiver pcb listen 
+                                                                   // to this port on selected ip (any)
+    // err_t is a lwip error type
     if (err != ERR_OK) {
-        xil_printf("UDP bind failed with error %d\n", err);
+        snprintf(tempstring, sizeof(tempstring),"UDP bind failed with error %d", err);
+        log_printer(tempstring);
         return;
     }
 
-    udp_recv(receiver_pcb, udp_receive_callback, NULL); // udp_recv calls udp_receive_callback with all its parameters from receiver_pcb
-    xil_printf("UDP receiver initialized on port %d\n", LISTEN_PORT);
+    udp_recv(receiver_pcb, udp_receive_callback, NULL); // udp_recv calls udp_receive_callback
+                                                        // with all its parameters from receiver_pcb
+
+    snprintf(tempstring, sizeof(tempstring),"UDP receiver initialized on port %d", LISTEN_PORT);
+    log_printer(tempstring);
 }
 
 void general_initialization() {
     ip_addr_t ipaddr, netmask, gw; // declaration of 3 variables ... ip_addr_t is a struct from lwIP
-    xil_printf("Starting lwIP UDP Receiver Example\n");
+    log_printer("Starting lwIP UDP Receiver Example");
 
-    IP4_ADDR(&ipaddr, 192, 168, 0, 27);    // board IP address
-    IP4_ADDR(&netmask, 255, 255, 255, 0);  // subnet mask
+    IP4_ADDR(&ipaddr, 169, 254, 201, 150);    // board IP address
+    IP4_ADDR(&netmask, 255, 255, 0, 0);  // subnet mask
     IP4_ADDR(&gw, 0, 0, 0, 0);             // gateway address
-    lwip_init(); // lwIP function that restarts everything there
+    lwip_init(); // lwIP function that initializes (resets internal data timers and protocols)
     struct netif *netif = &server_netif; // pointer to the global server_netif
-//  function the restarts the ethernet interface
-//  using the pointer it transfers that data into server_netif
-    if (!xemac_add(netif, &ipaddr, &netmask, &gw, mac_address, 0xe000b000)) {
-        xil_printf("Error adding network interface\n");
+                // will hold all information about the board network interface
+
+    if (!xemac_add(netif, &ipaddr, &netmask, &gw, mac_address, 0xe000b000)) {// adds an ethernet mac interface to lwip
+        log_printer("Error adding network interface");
         return;
     }
     netif_set_default(netif); // sets netif as the default network interface
     netif_set_up(netif); // marks the network interface as active
-    xil_printf("Link is %s\n", netif_is_link_up(netif) ? "up" : "down");
+    snprintf(tempstring, sizeof(tempstring),"Link is %s", netif_is_link_up(netif) ? "up" : "down");
+    log_printer(tempstring);
     print_ip("Board IP", &ipaddr);
 
+    		
+    // initialize gpios
     XGpio_Initialize(&gpio, XPAR_XGPIO_0_BASEADDR);
     XGpio_SetDataDirection(&gpio, 1, 0x00);
     XGpio_SetDataDirection(&gpio, 2, 0x00);
-    xil_printf("GPIOs initialized\n");
+    log_printer("GPIOs initialized");
 }
 
 
 int main() {
     general_initialization();
-    
+    log_printer("General Initialization function done");
+
     udp_receiver_init();
+    log_printer("UDP Receiver Initalization function done");
 
     while (1) {
-        xemacif_input(&server_netif);
+        xemacif_input(&server_netif);// checks for packets, puts it in pbuf and passes it down to netif
     }
     return 0;
 }
